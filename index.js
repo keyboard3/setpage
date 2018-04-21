4f0362dd3509ae4c4cf445ae4e09f3d46cff221d
gancy <keyboard3@icloud.com>
2018-04-21 20:01:55 +0800

    调整代码风格

--- index.js
+++ index.js
@@ -1,44 +1,41 @@
 #! /usr/bin/env node
-var fs = require("fs")
+let fs = require("fs");
 
-var userArgs = process.argv.splice(2).join('');
-if (userArgs === undefined || userArgs === null || userArgs===''){
+let userArgs = process.argv.splice(2).join('');
+if (typeof (userArgs) == 'undefined') {
 	console.log('page 参数不能为空！');
   return;
 }
-var path = process.cwd()+'/dist/app.json';
-var isExist=fs.existsSync(path);
-if(!isExist){
-  path = process.cwd()+'/app.json';
+
+let path = process.cwd() + '/dist/app.json';
+let isExist = fs.existsSync(path);
+if (!isExist) {
+  path = process.cwd() + '/app.json';
 }
-isExist=fs.existsSync(path);
-if(!isExist){
+isExist = fs.existsSync(path);
+
+if (!isExist) {
   console.log('当前项目目录找不到app.json文件');
   return;
 }
+
 setpage(path);
+function setpage(path) {
+  let data = fs.readFileSync(path);
+  let page = JSON.parse(data.toString());
+  let regx = new RegExp(userArgs,'i');
 
-function setpage(path){
-  var data = fs.readFileSync(path);
-  var page=JSON.parse(data.toString());
-
-  var regx = new RegExp(userArgs,'i');
-
-  var pos = page.pages.findIndex((element)=>{
-      return element!=null&&element.match(regx)!==null;
+  let pos = page.pages.findIndex((element) => {
+      return element != null && element.match(regx) !== null;
   });
 
-  var findItem = page.pages[pos];
+  let findItem = page.pages[pos];
   page.pages.splice(pos, 1);
-  if (pos!==-1){
-    console.log('find page:',findItem);
+  if (pos !== -1) {
+    console.log('find page:', findItem);
     page.pages.unshift(findItem);
-    fs.writeFile(path, JSON.stringify(page), function (err) {
-        if (err) {
-            return console.error(err);
-        }
-    });
+    fs.writeFile(path, JSON.stringify(page), err => err && console.error(err));
   }else{
-    console.log('not found page:',userArgs);
+    console.log('not found page:', userArgs);
   }
 }
